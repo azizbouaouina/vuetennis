52dd6232611db04697b6fbc99ff8566f1f625523
azizbouaouina <azizbouaouina010@gmail.com>
2024-06-28 14:53:06 +0200

    styling the not found page and adding the navbar

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,16 +1,68 @@
 <template>
+<div class="nav">
+    <NavBar :loggedIn="'true'"   />
+  </div>
   <div>
     <h1>Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
+    <p class="not-found">The page you are looking for does not exist.</p>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue'
+
+
 export default {
+    components:{
+        NavBar,
+    },
+    
   name: 'NotFound'
 }
 </script>
 
 <style>
-/* Add your styles here */
+.nav{
+  position: fixed;
+  top: 0;
+  left: 0;
+  color: white;  
+  z-index: 2;
+}
+
+body{
+    margin-top: 100px;
+    margin-left: 20px;
+    margin-right: 20px;
+}
+
+.not-found{
+  border: none;
+  border-radius: 4px;
+  color: #b1112c;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+  font-size: 20px; 
+  opacity: 0; 
+  animation: fadeIn 1s ease-out forwards; 
+
+}
+
+h1 {
+  color: #f00; 
+  font-weight: bold;
+  text-align: left; 
+  margin: 20px 0; 
+  font-family: 'Helvetica', Arial, sans-serif;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2); 
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0; 
+  }
+  to {
+    opacity: 1; 
+  }
+}
+
 </style>
